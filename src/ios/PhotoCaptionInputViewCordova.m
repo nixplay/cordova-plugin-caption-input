8abf757eb13f5102495a2922242187a78fb73600
James Kong <james.kong@nixplay.com>
2017-04-18 15:08:17 +0800

    remove delegate

--- src/ios/PhotoCaptionInputViewCordova.m
+++ src/ios/PhotoCaptionInputViewCordova.m
@@ -89,66 +89,66 @@
 #pragma mark - PhotoCaptionInputViewDelegate
 
 
-#pragma mark - MWPhotoBrowserDelegate
+// #pragma mark - MWPhotoBrowserDelegate
 
-- (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser {
-    return _photos.count;
-}
+// - (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser {
+//     return _photos.count;
+// }
 
-- (MWPhoto *)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index {
-    if (index < _photos.count)
-        return [_photos objectAtIndex:index];
-    return nil;
-}
-- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser thumbPhotoAtIndex:(NSUInteger)index{
+// - (MWPhoto *)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index {
+//     if (index < _photos.count)
+//         return [_photos objectAtIndex:index];
+//     return nil;
+// }
+// - (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser thumbPhotoAtIndex:(NSUInteger)index{
     
-    if([self.thumbnails count] > index ){
-       return  [self.thumbnails objectAtIndex:index];
-    }else{
-        return [self.photos objectAtIndex:index];
-    }
-}
-- (MWCaptionView *)photoBrowser:(MWPhotoBrowser *)photoBrowser captionViewForPhotoAtIndex:(NSUInteger)index {
-    MWPhoto *photo = [self.photos objectAtIndex:index];
-    MWCaptionView *captionView = [[MWCaptionView alloc] initWithPhoto:photo];
-    return captionView;
-}
+//     if([self.thumbnails count] > index ){
+//        return  [self.thumbnails objectAtIndex:index];
+//     }else{
+//         return [self.photos objectAtIndex:index];
+//     }
+// }
+// - (MWCaptionView *)photoBrowser:(MWPhotoBrowser *)photoBrowser captionViewForPhotoAtIndex:(NSUInteger)index {
+//     MWPhoto *photo = [self.photos objectAtIndex:index];
+//     MWCaptionView *captionView = [[MWCaptionView alloc] initWithPhoto:photo];
+//     return captionView;
+// }
 
-- (NSString *)photoBrowser:(MWPhotoBrowser *)photoBrowser titleForPhotoAtIndex:(NSUInteger)index{
-    return [NSString stringWithFormat:@"photo %lu",(unsigned long)index];
-}
-- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser didDisplayPhotoAtIndex:(NSUInteger)index{
-    NSLog(@"didDisplayPhotoAtIndex %lu", (unsigned long) index );
-}
-- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser actionButtonPressedForPhotoAtIndex:(NSUInteger)index{
-    NSLog(@"actionButtonPressedForPhotoAtIndex %lu", (unsigned long) index );
-}
-- (BOOL)photoBrowser:(MWPhotoBrowser *)photoBrowser isPhotoSelectedAtIndex:(NSUInteger)index{
-    NSLog(@"isPhotoSelectedAtIndex %lu", (unsigned long) index);
-    return NO;
-}
-- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index selectedChanged:(BOOL)selected{
-    NSLog(@"photoAtIndex %lu selectedChanged %i", (unsigned long) index , selected);
-    [self.selected_photos setValue: @((selected ? 1 : 0)) forKey:[NSString stringWithFormat:@"%lu", (unsigned long)index] ];
+// - (NSString *)photoBrowser:(MWPhotoBrowser *)photoBrowser titleForPhotoAtIndex:(NSUInteger)index{
+//     return [NSString stringWithFormat:@"photo %lu",(unsigned long)index];
+// }
+// - (void)photoBrowser:(MWPhotoBrowser *)photoBrowser didDisplayPhotoAtIndex:(NSUInteger)index{
+//     NSLog(@"didDisplayPhotoAtIndex %lu", (unsigned long) index );
+// }
+// - (void)photoBrowser:(MWPhotoBrowser *)photoBrowser actionButtonPressedForPhotoAtIndex:(NSUInteger)index{
+//     NSLog(@"actionButtonPressedForPhotoAtIndex %lu", (unsigned long) index );
+// }
+// - (BOOL)photoBrowser:(MWPhotoBrowser *)photoBrowser isPhotoSelectedAtIndex:(NSUInteger)index{
+//     NSLog(@"isPhotoSelectedAtIndex %lu", (unsigned long) index);
+//     return NO;
+// }
+// - (void)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index selectedChanged:(BOOL)selected{
+//     NSLog(@"photoAtIndex %lu selectedChanged %i", (unsigned long) index , selected);
+//     [self.selected_photos setValue: @((selected ? 1 : 0)) forKey:[NSString stringWithFormat:@"%lu", (unsigned long)index] ];
     
-}
-- (void)photoBrowserDidFinishModalPresentation:(MWPhotoBrowser *)photoBrowser{
-    NSLog(@"photoBrowserDidFinishModalPresentation");
-    __block NSMutableArray *fileStrings = [[NSMutableArray alloc] init];
-    __block CDVPluginResult* result = nil;
+// }
+// - (void)photoBrowserDidFinishModalPresentation:(MWPhotoBrowser *)photoBrowser{
+//     NSLog(@"photoBrowserDidFinishModalPresentation");
+//     __block NSMutableArray *fileStrings = [[NSMutableArray alloc] init];
+//     __block CDVPluginResult* result = nil;
     
-    [self.selected_photos enumerateKeysAndObjectsUsingBlock:^(NSString* key, id  _Nonnull obj, BOOL * _Nonnull stop) {
-        BOOL selected = (BOOL)obj;
-        if(selected){
-            MWPhoto * photo = [self.photos objectAtIndex:[key integerValue]];
-            [fileStrings addObject: [photo url] ];
-        }
-    }];
+//     [self.selected_photos enumerateKeysAndObjectsUsingBlock:^(NSString* key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+//         BOOL selected = (BOOL)obj;
+//         if(selected){
+//             MWPhoto * photo = [self.photos objectAtIndex:[key integerValue]];
+//             [fileStrings addObject: [photo url] ];
+//         }
+//     }];
     
     
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: [NSDictionary dictionaryWithObjectsAndKeys: fileStrings, @"images", nil]];
-    [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    [self.viewController dismissViewControllerAnimated:YES completion:nil];
-}
+//     result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: [NSDictionary dictionaryWithObjectsAndKeys: fileStrings, @"images", nil]];
+//     [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+//     [self.viewController dismissViewControllerAnimated:YES completion:nil];
+// }
 
 @end
