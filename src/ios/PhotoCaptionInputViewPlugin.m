7b1b07bf1095189fb0e7114ac6f0d647859ba974
James Kong <james.kong@nixplay.com>
2018-02-22 16:36:08 +0800

    Added location indicator label

--- src/ios/PhotoCaptionInputViewPlugin.m
+++ src/ios/PhotoCaptionInputViewPlugin.m
@@ -199,7 +199,7 @@
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:controller.view animated:YES];
     hud.mode = MBProgressHUDModeAnnularDeterminate;
     hud.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.3];
-    hud.label.text = NSLocalizedString(@"LOADING", nil);
+    hud.label.text = NSLocalizedString(@"OPTIMIZING", nil);
 
     dispatch_group_async(dispatchGroup, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         __block CGFloat numberOfImage = [photos count];
@@ -245,7 +245,7 @@
               dispatch_async(dispatch_get_main_queue(), ^{
                   //                  [progressView setProgress:((CGFloat)index/(CGFloat)numberOfImage)+((progress/100.0f)*(1/(CGFloat)numberOfImage))];
                   float outputprogress = ((CGFloat)index/(CGFloat)numberOfImage)+((progress/100.0f)*(1/(CGFloat)numberOfImage));
-                  hud.label.text = [NSString stringWithFormat:@"%.f%%",outputprogress*100];
+//                  hud.label.text = [NSString stringWithFormat:@"%.f%%",outputprogress*100];
                   hud.progress = outputprogress;
               });
           } errorCallback:^(CDVPluginResult *errorResult) {
